fcb2369542c44cdfc585b0802138ffe6ea0987ac
john_ <john_@142.232.50.122>
2016-01-28 14:58:35 -0800

    Finish up tutorial 2.

--- application/controllers/About.php
+++ application/controllers/About.php
@@ -1,7 +1,7 @@
 <?php
 //defined('BASEPATH') OR exit('No direct script access allowed');
 
-class About extends CI_Controller {
+class About extends Application {
 
 	public function index()
 	{
--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -2,10 +2,13 @@
 
 //defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Welcome extends Application {
 
 	public function index()
 	{
-		$this->load->view('welcome');
+		//$this->load->view('welcome');
+                
+                $this->data['pagebody'] = 'welcome';
+                $this->render();
 	}
 }
